e2e8853e9433d69b1d0e952c501f3e160e685024
Marc Chantreux <khatar@phear.org>
2014-10-29 23:21:33 +0100

    fix atom generation

--- posts/2014/gen.zsh
+++ posts/2014/gen.zsh
@@ -2,7 +2,8 @@ while {read created file title} {
     local id="tag:eiro.github.com,${created#T*}:$file"
     print "<entry><id>$id</id><title>$title</title>"
     print '<content type="html">'
-    pandoc -s -thtml5 $file | perl -MHTML::Entities -lne 'print encode_entities $_'
+    pandoc -thtml5 $file | perl -MHTML::Entities -lne 'print encode_entities $_'
+    print '</content>'
     print '<link rel="alternate"
         type="text/html" href="http://eiro.github.com/aposts/2014/'${file%.md}.html'/>'
     print "<published>$created</published><updated>$created</updated></entry>"
