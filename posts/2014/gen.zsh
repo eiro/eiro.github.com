c6b07f06e01fad93557f0c345cf0ab763be4858c
Marc Chantreux <khatar@phear.org>
2014-10-29 23:51:33 +0100

    a valid atom file from m4 template.m4

--- posts/2014/gen.zsh
+++ posts/2014/gen.zsh
@@ -1,8 +1,8 @@
 while {read created file title} {
-    local id="tag:eiro.github.com,${created#T*}:$file"
+    local id="tag:eiro.github.com,${created%T*}:$file"
     print "<entry><id>$id</id><title>$title</title>"
     print '<content type="html">'
-    pandoc -thtml5 $file | perl -MHTML::Entities -lne 'print encode_entities $_'
+    pandoc -thtml5 $file | perl -CSD -MHTML::Entities -lne 'print encode_entities $_,q(<>&)'
     print '</content>'
     print '<link rel="alternate"
         type="text/html" href="http://eiro.github.com/aposts/2014/'${file%.md}.html'"/>'
