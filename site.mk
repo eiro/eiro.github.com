759b34323b75a606ade1b7f65d87cab349ca4bf4
Marc Chantreux <khatar@phear.org>
2015-01-02 00:57:23 +0100

    welcome bin/post_menu
    * add_comment_links.pl was a kudge, replaced by the new shiny
      bin/post_menu
    * Makefiles changed to use it

--- site.mk
+++ site.mk
@@ -3,10 +3,11 @@
 
 mdpages  = $(wildcard *.md)
 webpages = ${mdpages:.md=.html}
-
 main_html_template=$(depth)/template.html5
 htmlify      = pandoc -Vdepth=$(depth) --toc --template $(main_html_template)
 can_comment  = perl $(depth)/add_comment_links.pl
 
+buildpage    = $(htmlify) -s -B menu -o
+
 .md.html: $(main_html_template) menu
-	$(htmlify) -s -B menu -o $@ $<
+	$(buildpage) $@ $<
