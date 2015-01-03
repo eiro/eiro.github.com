428fb43febe8b904e8ab235429508107e7d04b1c
Marc Chantreux <khatar@phear.org>
2015-01-03 09:31:06 +0100

    remove irrelevant tests on py13 slides

--- py13/do.sh
+++
@@ -1,6 +0,0 @@
-perl -lpe '
-    BEGIN {%l = qw( py Python p Perl r Ruby )}
-    s/^<[.](p|py|r)$/<pre><code data-trim class="writtenIn $l{$1}">/;
-    s#^>$#</code></pre>#
-' < slides.md | pandoc -s -trevealjs --template template.reveal -o p.html
-
