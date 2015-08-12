bfb1e3acb3591bb277a7712195416cf66c6baf98
Marc Chantreux <khatar@phear.org>
2015-08-12 14:06:35 +0200

    lot of defs added
    some can have arguments so i added a test suite: test_defs.zsh

---
+++ test_defs.zsh
@@ -0,0 +1,23 @@
+uzu devel/TAP :all
+
+render () { m4 defs - <<< "$*" }
+
+macros_are_working () {
+    local desc macro got expected
+
+    for desc macro expected (
+
+    '_PERLDOCSEC with 2 args'
+    '_PERLDOCSEC(perlsub,Signatures)'
+    '[Signatures](http://perldoc.perl.org/perlsub.html#Signatures)'
+
+    '_PERLDOCSEC with 3 args'
+    '_PERLDOCSEC(perlsub,Signatures,signatures introduced in perl 5.20)'
+    '[signatures introduced in perl 5.20](http://perldoc.perl.org/perlsub.html#Signatures)'
+
+    ) { test "$(render $macro)" = "$expected"; ok $desc }
+}
+
+
+prove macros_are_working
+
